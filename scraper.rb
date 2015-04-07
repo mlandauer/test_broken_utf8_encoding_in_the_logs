6b4eb7390a793f99661fe6c874dfae2666614b0d
Matthew Landauer <matthew@oaf.org.au>
2015-04-07 15:51:47 +1000

    This should be the bit that breaks things

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,3 @@
-puts "Hello!"
-a = "Contra toda autoridad!...excepto mi mamá!"
-puts a
-puts a.encoding
-a.force_encoding("ASCII-8BIT")
-puts a
-puts a.encoding
 b = "\xC0"
-# puts b
-# puts b.encoding
+puts b
+puts b.encoding
