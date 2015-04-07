16c75ede63f97d0f523f2aee1f4487e16fedc357
Matthew Landauer <matthew@oaf.org.au>
2015-04-07 15:46:13 +1000

    Print out a few strings

--- scraper.rb
+++ scraper.rb
@@ -1,24 +1,10 @@
-# This is a template for a Ruby scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
-
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the Mechanize or ScraperWiki libraries. You can use whatever gems are installed
-# on Morph for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+puts "Hello!"
+a = "Contra toda autoridad!...excepto mi mamá!"
+puts a
+puts a.encoding
+a.force_encoding("ASCII-8BIT")
+puts a
+puts a.encoding
+b = "\xC0"
+puts b
+puts b.encoding
